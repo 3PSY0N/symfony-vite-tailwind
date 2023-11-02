fe335d2e5282e625c0573258d5ac049fbcd4d57f
3PSY0N <78256817+3PSY0N@users.noreply.github.com>
2023-11-02 22:44:31 +0100

    Add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.{twig,html.twig}", "./assets/js/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
